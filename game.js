3484d9cc7e773cfef181ed748d4bc7419e00ce3b
asteriskman7 <asteriskman@gmail.com>
2018-01-20 21:14:25 -0500

    get physics set up

--- game.js
+++ game.js
@@ -8,6 +8,8 @@ let game = {
   titleMode: undefined,
   buttons: undefined,
   mousePos: undefined,
+  world: undefined,
+  scale: undefined,
   init: function() {
     console.log('init');
 
@@ -23,6 +25,7 @@ let game = {
     game.buttons = [];
     game.mousePos = {x: 0, y: 0};
     game.hoverColor = '#11111110';
+    game.scale = 75; //75 pixels per meter
 
     game.createButton(game.canvas.width * 0.5 - 120, game.canvas.height * 0.66 - 20, 240, 40,
       "30px 'Russo One'", '#FFFF00', '#0000FF', 'START GAME', game.startGame);
@@ -58,19 +61,55 @@ let game = {
     window.requestAnimationFrame(game.tick);
   },
   update: function(timestamp, delta) {
-
-  },
-  draw: function(timestamp, delta) {
-    game.ctx.save();
-    game.ctx.clearRect(0,0,600,600);
     if (game.titleMode) {
-      game.ctx.font = "50px 'Russo One'";
-      game.ctx.textAlign = 'center';
-      game.ctx.textBaseline = 'middle';
-
-      game.ctx.fillText('S.P.O.T & B.O.S.S', game.canvas.width * 0.5, game.canvas.height * 0.3);
 
     } else {
+      game.world.Step(1/60, 2, 2);
+      game.world.ClearForces();
+    }
+  },
+  draw: function(timestamp, delta) {
+    let ctx = game.ctx;
+    ctx.save();
+    ctx.clearRect(0,0,600,600);
+    if (game.titleMode) {
+      ctx.font = "50px 'Russo One'";
+      ctx.textAlign = 'center';
+      ctx.textBaseline = 'middle';
+
+      ctx.fillText('S.P.O.T & B.O.S.S', game.canvas.width * 0.5, game.canvas.height * 0.3);
+
+    } else {
+      for (let b = game.world.GetBodyList(); b; b = b.m_next) {
+        let userData = b.GetUserData();
+        if (userData === null) {
+          userData = {type: 'none'};
+        }
+        let pos = b.GetPosition();
+        ctx.save();
+        ctx.translate(pos.x * game.scale, pos.y * game.scale);
+        ctx.rotate(b.GetAngle());
+        ctx.translate(-pos.x * game.scale, -pos.y * game.scale);
+        switch (userData.type) {
+          case 'rect':
+            ctx.fillStyle = "#00FF00";
+
+
+            ctx.fillRect(
+              (pos.x - userData.width / 2) * game.scale,
+              (pos.y - userData.height / 2) * game.scale,
+              userData.width * game.scale,
+              userData.height * game.scale
+            );
+            break;
+          case 'ground':
+            break;
+          default:
+            throw `Unknown body type ${userData.type}`;
+        }
+
+        ctx.restore();
+      }
 
     }
 
@@ -81,6 +120,17 @@ let game = {
   startGame: function() {
     game.buttons = [];
     game.titleMode = false;
+
+    //set up Box2D
+    game.world = new b2World(new b2Vec2(0, 10), true);
+    game.world.GetBodyList().SetUserData({type: 'ground'}); //not sure what this line does
+
+    //create walls
+    let floor = game.createWall(0, (game.canvas.height - 10) / game.scale, game.canvas.width / game.scale, 20 / game.scale);
+    let leftWall = game.createWall(-10 / game.scale, 0, 20 / game.scale, game.canvas.height / game.scale);
+    //create spot
+    game.spot = game.createBox(200 / game.scale, 200 / game.scale, 0.5, 0.5);
+    //create cord
   },
   createButton: function(x, y, w, h, font, bgcolor, fgcolor, text, callback) {
     //x,y are the upper left corner
@@ -136,7 +186,77 @@ let game = {
   canvasMouseMove: function(event) {
     let pos = game.getCursorPosition(event);
     game.mousePos = pos;
-  }
+  },
+  createWall: function(x, y, width, height) {
+    //x,y are the upper left corners
+    var userData = {};
+    userData.type = 'rect';
+    userData.width = width;
+    userData.height = height;
+
+    var fDef = new b2FixtureDef();
+    fDef.density = 1.0;
+    fDef.friction = 0.5;
+    fDef.friction = 0.0;
+    fDef.restitution = 0.2;
+    fDef.restitution = 0.1;
+    fDef.shape = new b2PolygonShape();
+    fDef.shape.SetAsBox(width / 2, height / 2);
+
+
+    var bDef = new b2BodyDef();
+    bDef.type = b2Body.b2_staticBody;
+
+    bDef.position.x = x + width * 0.5;
+    bDef.position.y = y + height * 0.5;
+
+    var newBody;
+    newBody = game.world.CreateBody(bDef);
+    newBody.CreateFixture(fDef);
+    newBody.SetUserData(userData);
+
+    return newBody;
+  },
+  createBox: function(x, y, width, height) {
+    var userData = {};
+    userData.type = 'rect';
+    userData.width = width;
+    userData.height = height;
+
+    var fDef = new b2FixtureDef();
+    fDef.density = 1.0;
+    fDef.friction = 0.5;
+    fDef.friction = 0.0;
+    fDef.restitution = 0.2;
+    fDef.restitution = 0.1;
+    fDef.shape = new b2PolygonShape();
+    fDef.shape.SetAsBox(width / 2, height / 2);
+
+    var bDef = new b2BodyDef();
+    bDef.type = b2Body.b2_dynamicBody;
+    bDef.position.x = x + width * 0.5;
+    bDef.position.y = y + height * 0.5;
+
+    var newBody;
+    newBody = game.world.CreateBody(bDef);
+    newBody.CreateFixture(fDef);
+    newBody.SetUserData(userData);
+
+    return newBody;
+  },
 };
+
+//bring Box2D items into global namespace
+let b2Vec2 = Box2D.Common.Math.b2Vec2,
+  b2BodyDef = Box2D.Dynamics.b2BodyDef,
+  b2Body = Box2D.Dynamics.b2Body,
+  b2FixtureDef = Box2D.Dynamics.b2FixtureDef,
+  b2Fixture = Box2D.Dynamics.b2Fixture,
+  b2World = Box2D.Dynamics.b2World,
+ 	b2MassData = Box2D.Collision.Shapes.b2MassData,
+ 	b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape,
+ 	b2CircleShape = Box2D.Collision.Shapes.b2CircleShape,
+	b2DebugDraw = Box2D.Dynamics.b2DebugDraw,
+  b2RevoluteJointDef =  Box2D.Dynamics.Joints.b2RevoluteJointDef;
 
 game.init();
